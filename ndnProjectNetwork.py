fba70ceeaa16eeba478255eb24c47f9bac92df24
Isla Duncan <iduncan2026@francisparker.org>
2024-07-08 12:21:47 -0700

    Forgot to add network file to the previous commit, added length attribute to edges

--- ndnProjectNetwork.py
+++ ndnProjectNetwork.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from numpy import random
 def graphConfiguration():
     G = nx.Graph()
     nodes = []
@@ -21,18 +22,20 @@ def graphConfiguration():
         channels.append(i+3)
     nodes.append(9)
     G.add_nodes_from(nodes)
-    G.add_edge(0, 1, object = 1)
-    G.add_edge(1, 2, object = 2)
-    G.add_edge(2, 9, object = 3)
-    G.add_edge(3, 4, object = 5)
-    G.add_edge(4, 5, object = 6)
-    G.add_edge(5, 9, object = 7)
-    G.add_edge(6, 7, object = 9)
-    G.add_edge(7, 8, object = 10)
-    G.add_edge(8, 9, object = 11)
+    G.add_edge(0, 1, id = 1, length = random.randint(10, 1000))
+    G.add_edge(1, 2, id = 2, length = random.randint(10, 1000))
+    G.add_edge(2, 9, id = 3, length = random.randint(10, 1000))
+    G.add_edge(3, 4, id = 5, length = random.randint(10, 1000))
+    G.add_edge(4, 5, id = 6, length = random.randint(10, 1000))
+    G.add_edge(5, 9, id = 7, length = random.randint(10, 1000))
+    G.add_edge(6, 7, id = 9, length = random.randint(10, 1000))
+    G.add_edge(7, 8, id = 10, length = random.randint(10, 1000))
+    G.add_edge(8, 9, id = 11, length = random.randint(10, 1000))
     edgeChannels.append([0, 0])
     edgeChannels.append([4, 3])
     edgeChannels.append([8, 6])
+    for x in edgeChannels:
+        x.append(random.randint(500, 2000))
     data_producers = [9]
     results = {"graph": G, "edgeChannels": edgeChannels, "dataProducers": data_producers}
     return results
